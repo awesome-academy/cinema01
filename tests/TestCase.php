292936dd9df5d2c2c402b39c3b2a754dd8bd4fe6
Tuan Anh <ahxtah1996@gmail.com>
2019-05-22 10:56:29 +0700

    12530 unitTest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use Faker\Factory as Faker;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $faker;
+
+    public function setUp(): void
+    {
+    	parent::setUp();
+    	$this->faker = Faker::create();
+    }
 }
